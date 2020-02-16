27d022b40b7903ce563990e88bba8d7758fd39ab
Seth Holobaugh <seth.holobaugh@gmail.com>
2020-02-16 02:30:27 -0600

    Created Footer.vue

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <NavBar/>
     <router-view />
+    <Footer/>
   </div>
 </template>
 
 <script>
 import NavBar from './components/NavBar'
+import Footer from './components/Footer'
 
 export default {
   name: "MainApp",
   components: {
-    NavBar
+    NavBar,
+    Footer
   }
 };
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="flex justify-center items-center">
+        <div class="h-20 bottom-o py-8 flex">
+            <p class="pr-2">&copy; Seth Holobaugh</p>
+            <a class="github" href="https://github.com/swholobaugh" target="_blank"><GithubCircle /></a>
+            <a class="facebook" href="https://facebook.com" target="_blank"><Facbook /></a>
+            <a class="twitter" href="https://twitter.com" target="_blank"><Twitter /></a>
+        </div>
+    </div>
+</template>
+
+<script>
+import GithubCircle from 'vue-material-design-icons/GithubCircle'
+import Facebook from 'vue-material-design-icons/Facebook'
+import Twitter from 'vue-material-design-icons/Twitter'
+
+export default {
+    name:'Footer',
+    components: {
+        GithubCircle,
+        Facebook,
+        Twitter
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.github:hover {
+    @apply text-red-700;
+}
+.facebook:hover {
+    @apply text-blue-700;
+}
+.twitter:hover {
+    @apply text-blue-400;
+}
+
+</style>
