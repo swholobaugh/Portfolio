e201c30083c432d2b87d31159f16e5120557415d
Seth Holobaugh <seth.holobaugh@gmail.com>
2020-02-16 01:41:51 -0600

    Added NavBar and Home

---
+++ src/components/NavBar.vue
@@ -0,0 +1,32 @@
+<template>
+    <nav class="nav w-screen z-20 shadow-2xl bg-red-800 text-white">
+        <div class="left">
+            <span class="font-light text-3xl">My
+                <span class="font-semibold">Portfolio</span>
+            </span>
+        </div>
+        <div class="right">
+            <router-link class="link" to="/">Home</router-link>
+            <router-link class="link" to="/portfolio">Portfolio</router-link>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'NavBar'
+}
+</script>
+
+
+<style lang="scss" scoped>
+.nav {
+    @apply h-24 fixed px-12 flex justify-between items-center;
+}
+.link {
+    @apply px-2 font-semibold;
+    &:hover{
+        @apply text-yellow-500;
+    }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,26 @@
 <template>
-  <div class="home">
-    <h1>Hello There</h1>
+  <div>
+    <div class="relative h-screen w-screen bg-cover bg-center shadow-2xl" 
+    :style="`background-image:url(${bgImage})`">
+
+      <div class="h-screen w-screen bg-red-800 opacity-75"/>
+      <div class="absolute h-screen w-screen top-0 flex flex-col justify-center items-center">
+          <h1 class="text-6xl font-bold text-white px-4 hover:bg-white hover:text-red-800 opacity-75">Seth Holobaugh</h1>
+          <h3 class="text-3xl font-bold text-white px-4 hover:bg-white hover:text-red-800 opacity-75">Student/Developer</h3>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import bgImage from '../assets/bgimage.jpg'
+
 export default {
-  name: "Home"
+  name: "Home",
+  data(){
+    return {
+      bgImage
+    }
+  }
 };
 </script>
- 
